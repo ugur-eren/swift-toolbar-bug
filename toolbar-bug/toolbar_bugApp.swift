f9f98ba017fffddc43b64cc6d76a9187b5246097
Uğur Eren <contact@ugureren.net>
2022-06-02 01:24:30 +0300

    Initial Commit

---
+++ toolbar-bug/toolbar_bugApp.swift
@@ -0,0 +1,17 @@
+//
+//  toolbar_bugApp.swift
+//  toolbar-bug
+//
+//  Created by Uğur Eren on 2.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct toolbar_bugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
